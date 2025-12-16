7e50131dfbfef8c8a40eef66f8f6f286915841e0
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2025-12-16 12:34:13 +0330

    add base user.test.tsx

---
+++ packages/ctablex-core/src/user.test.tsx
@@ -0,0 +1,5 @@
+import { render, renderHook, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import {} from './index'
+
+describe('user test - exploring @ctablex/core as a new user', () => {})
