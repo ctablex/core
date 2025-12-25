3d1f9473192f0aea400c7fd2f3ad8a18600c320b
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2025-12-26 00:29:33 +0330

    feat(table): export Cell and HeaderCell components with their props definitions (#10)

--- packages/ctablex-table/index.d.ts
+++ packages/ctablex-table/index.d.ts
@@ -10,6 +10,14 @@ import { NullableContentProps } from '@ctablex/core';
 import { ReactElement } from 'react';
 import { ReactNode } from 'react';
 
+export declare function Cell<D>(props: CellProps<D>): JSX_2.Element;
+
+export declare interface CellProps<D> {
+  accessor?: Accessor<D>;
+  children?: ReactNode;
+  el?: ReactElement;
+}
+
 export declare function Column<D = any>(props: ColumnProps<D>): JSX_2.Element;
 
 export declare interface ColumnProps<D = any> {
@@ -45,6 +53,15 @@ export declare interface DataTableProps<D> {
 export { DefaultContent };
 
 export declare const defaultTableElements: TableElements;
+
+export declare function HeaderCell(
+  props: HeaderCellProps,
+): ReactElement<any, string | JSXElementConstructor<any>>;
+
+export declare interface HeaderCellProps {
+  children?: ReactNode;
+  el?: ReactElement;
+}
 
 export { NullableContent };
 
